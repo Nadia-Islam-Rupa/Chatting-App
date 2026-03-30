b3e6dd792c48446a3765f8e9051b438a9250df4c
Nadia Islam <sohasara853@gmail.com>
2026-03-30 08:19:11 +0600

    Remove Firebase initialization from main.dart and related import

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:chatting_application/firebase_options.dart';
 import 'package:chatting_application/routes/app_route.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
