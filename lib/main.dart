8449f40757ec8b04dbf0cd5324fff798c368aee5
Nadia Islam <sohasara853@gmail.com>
2026-04-06 20:43:51 +0600

    Integrate Supabase and URL Launcher plugins; update pubspec.yaml and generated files

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:chatting_application/services/routes/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://mxgdkgunszcfxqhsutqy.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14Z2RrZ3Vuc3pjZnhxaHN1dHF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NzkxMDAsImV4cCI6MjA5MTA1NTEwMH0.nTxvnAAl4guAd_jdJUSMZxLCKrdlwTnSC6N9bhBfgbA',
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
